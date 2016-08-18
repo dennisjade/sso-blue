40eabad0ac8fd1517eeedb956941c78fc44c6d66
Dennis Jade Toribio <dennisjade@denniss-mbp.sg.ibm.com>
2016-08-18 09:40:33 +0800

    initial

---
+++ settings.js
@@ -0,0 +1,14 @@
+// exports.client_id = "ZjNkMWYxZDItZTY1OS00";
+// exports.client_secret = "MjQyMTc2OTYtNGI3NS00";
+// exports.authorization_url = "https://prepiam.toronto.ca.ibm.com/idaas/oidc/endpoint/default/authorize"; 
+// exports.token_url = "https://prepiam.toronto.ca.ibm.com/idaas/oidc/endpoint/default/token"; 
+// exports.issuer_id = "https://prepiam.toronto.ca.ibm.com"; 
+// exports.callback_url = "https://localhost:5000/auth/sso/callback";      
+
+
+exports.client_id = "ZjNkMWYxZDItZTY1OS00";
+exports.client_secret = "MjQyMTc2OTYtNGI3NS00";
+exports.authorization_url = "https://prepiam.toronto.ca.ibm.com/idaas/oidc/endpoint/default/authorize"; 
+exports.token_url = "https://prepiam.toronto.ca.ibm.com/idaas/oidc/endpoint/default/token"; 
+exports.issuer_id = "https://prepiam.toronto.ca.ibm.com"; 
+exports.callback_url = "https://blueid-test.w3ibm.mybluemix.net/auth/sso/callback";
